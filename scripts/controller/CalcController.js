fb547e43c97671623bb9050bd3c750a20b025847
ANA CAROLINA TARGINO PRAXEDES <07751228407@sesed.interno>
2022-03-21 16:59:31 -0300

    inicio de logica da calculadora com switch

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -1,6 +1,8 @@
 class CalcController{
     
     constructor(){
+        this._operation = [];
+        //estou guardando a operação que vai acontecer
         this._locale = 'pt-BR'
         this._displayCalcEl = document.querySelector("#display");
         this._dateEl = document.querySelector("#data");
@@ -25,19 +27,107 @@ class CalcController{
 
     }
 
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            //ele está passando cada um dos eventos
+            element.addEventListener(event,fn,false);
+                    //quando o evento for disparado, o false vai fazer que ele so aconteça uma vez
+        });
+    }
+
+    //métodos de ações para a calculadora
+    clearAll(){
+        this._operation = [];
+        //vai esvaziar o array
+    }
+
+    clearEntry(){
+        this._operation.pop();
+        //o pop vai apagar o ultimo elemento do array
+        
+    }
+
+    addOperation(value){
+        this._operation.push(value);
+        //push adiciona mais um elemento no final do array
+        //tudo que for numero, ele adiciona
+        console.log(this._operation);
+    }
+    
+    setError(){
+        this.displayCalc = "Error";
+    }
+
+
+
+    //lógica da calculadora
+    execBtn(value){
+        switch(value){
+            //ac = limpa tudo
+            case 'ac':
+
+            break;
+            //ce = cancela a ultima entrada
+            case 'ce':
+
+            break;
+            case 'soma':
+
+            break;
+            case 'subtracao':
+
+            break;
+            case 'divisao':
+
+            break;
+            case 'multiplicacao':
+
+            break;
+            case 'porcento':
+
+            break;
+            case 'igual':
+
+            break;
+            case '0':
+            case '1':
+            case '2':
+            case '3':
+            case '4':
+            case '5':
+            case '6':
+            case '7':
+            case '8':
+            case '9':    
+                this.addOperation(parseInt(value));
+            break;
+
+            default:
+                this.setError();
+            break;
+        }               
+    }
+
     initButtonsEvents(){
         //eventos do botões
         let buttons = document.querySelectorAll("#buttons > g, #parts > g");
 
 
         buttons.forEach((btn,index)=>{
-            //ele vai percorrer para cada botão que encontrar na lista.
-            // Vai receber o botão da vez e adiciono o evento no botão
-            btn.addEventListener('click',e=>{
-                console.log(btn.className.baseVal);
-            });
-        })
 
+            this.addEventListenerAll(btn,'click drag ',e =>{
+                let textBtn = console.log(btn.className.baseVal);
+
+                this.execBtn(textBtn);
+                //Vai executar a ação desse botão. A ação que vai ser tomada 
+            //está sendo decidida pelo o switch
+            });
+            this.addEventListenerAll(btn,'mouseover mouseup mousedown',e =>{
+                btn.style.cursor = "pointer";
+                // o mouse estiver em cima do btn, o cursor mudará
+            });
+
+        })
     }
 
     setDisplayDateTime(){
