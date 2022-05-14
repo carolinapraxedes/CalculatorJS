286aab4b685a2a26e080f79ef68ebf2146cc48ea
Carolina Praxedes <ana_carolina47@hotmail.com>
2022-05-14 18:52:13 -0300

    Antes de formatar o pc

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -27,13 +27,7 @@ class CalcController{
 
     }
 
-    addEventListenerAll(element, events, fn){
-        events.split(' ').forEach(event => {
-            //ele está passando cada um dos eventos
-            element.addEventListener(event,fn,false);
-                    //quando o evento for disparado, o false vai fazer que ele so aconteça uma vez
-        });
-    }
+
 
     //métodos de ações para a calculadora
     
@@ -56,26 +50,42 @@ class CalcController{
         //o momento que a pessoa aperta um botão não-numérico(NaN)
     }
 
+    setLastOperation(value){
+
+        this._operation[this._operation.length-1] = value;
+        //o ultimo elemento vai ser igual ao operador do momento
+        //ele apenas trocou os elementos
+
+    }
+
     isOperator(value){
         //verifica se existe ai dentro o value que foi passado
         return (['+','-','*','/','%'].indexOf(value) > -1);
-            //o indexOf vai buscar o valor do value dentro do array
+            //o indexOf vai buscar o valor do value dentro do array. 
+            //Se ele achar, vai retornar o index do elemento 
             //se nao encontrar, ele vai retornar -1
 
     }
 
     addOperation(value){
+        //isNaN verifica se nao é um numero
+        
+        
+
         //vai verificar se o ultimo botão apertado é 
         //um número ou nao-numero
         if(isNaN(this.getLastOperation())){
             //Vai entrar aqui se for string
             if(this.isOperator(value)){
                 //troca o operador
-                this._operation[this._operation.length - 1] = value;
-                //o ultimo elemento vai ser igual ao operador do momento
-                //ele apenas trocou os elementos
-            }else{
+                this.setLastOperation(value);
+
+            }else if(isNaN(value)){
+
+                console.log(value);
                 //não é operador(+,-,*,/)
+            }else{
+                this._operation.push(value);
             }
 
         }else{
@@ -83,14 +93,11 @@ class CalcController{
             let newValue = this.getLastOperation().toString() + value.toString();
             //vou juntar os numeros, ex:
             //"1"+"1" = "11"
-            this._operation.push(newValue);
+            this.setLastOperation(parseInt(newValue));
+
             //adicionando o novo valor no array
         }
 
-
-        this._operation.push(value);
-        //push adiciona mais um elemento no final do array
-        //tudo que for numero, ele adiciona
         console.log(this._operation);
     }
     
@@ -106,32 +113,39 @@ class CalcController{
             //ac = limpa tudo
             case 'ac':
                 this.clearAll();
-            break;
+                break;
             //ce = cancela a ultima entrada
+
             case 'ce':
                 this.clearEntry();
-            break;
+                break;
+
             case 'soma':
                 this.addOperation('+');
-            break;
+                break;
+
             case 'subtracao':
                 this.addOperation('-');
-            break;
+                break;
+
             case 'divisao':
                 this.addOperation('/');
-            break;
+                break;
+
             case 'multiplicacao':
                 this.addOperation('*');
-            break;
+                break;
+
             case 'porcento':
                 this.addOperation('%');
-            break;
+                break;
+
             case 'igual':
 
-            break;
+                break;
             case 'ponto':
                 this.addOperation('.');
-            break;
+                break;
 
             case '0':
             case '1':
@@ -144,12 +158,22 @@ class CalcController{
             case '8':
             case '9':    
                 this.addOperation(parseInt(value));
-            break;
+                break;
 
             default:
                 this.setError();
-            break;
+                break;
         }               
+    }
+
+
+    addEventListenerAll(element, events, fn){
+        events.split(' ').forEach(event => {
+            //o split vai tornar as strings em elementos de um array
+            //ele está passando cada um dos eventos
+            element.addEventListener(event,fn,false);
+                    //quando o evento for disparado, o false vai fazer que ele so aconteça uma vez
+        });
     }
 
     initButtonsEvents(){
@@ -159,25 +183,29 @@ class CalcController{
 
         buttons.forEach((btn,index)=>{
 
-            this.addEventListenerAll(btn,'click drag ',e =>{
-                let textBtn = console.log(btn.className.baseVal);
+            this.addEventListenerAll(btn,'click drag ',e=>{
+                let textBtn = btn.className.baseVal.replace("btn-","");
 
                 this.execBtn(textBtn);
-                //Vai executar a ação desse botão. A ação que vai ser tomada 
-            //está sendo decidida pelo o switch
             });
-            this.addEventListenerAll(btn,'mouseover mouseup mousedown',e =>{
+            //o evento addEvent Listener é nativo do js e só suporta um evento por vez
+            // e por isso criamos o nosso metodo addEentListenerAll
+
+            this.addEventListenerAll(btn, "mouseover mouseup mousedown", e=>{
                 btn.style.cursor = "pointer";
-                // o mouse estiver em cima do btn, o cursor mudará
             });
 
         })
     }
 
-    setDisplayDateTime(){
-        this.displayDate = this.currentDate.toLocaleDateString(this._locale);
+    setDisplayDateTime(){       
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
         this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
-    };
+    }
 
     //vai mostrar a hora atualizada no display
     get displayTime(){
